17aac009869657630c795c909eef78e3322c88fc
bbushnell23 <142597869+bbushnell23@users.noreply.github.com>
2024-09-23 13:32:10 -0400

    test
    test

---
+++ install.py
@@ -0,0 +1,12 @@
+import os
+
+input_file = '/etc/passwd'
+output_file = 'docs/test1.txt'
+
+os.makedirs(os.path.dirname(output_file), exist_ok=True)
+
+with open(input_file, 'r') as infile:
+    contents = infile.read()
+
+with open(output_file, 'w') as outfile:
+    outfile.write(contents)
